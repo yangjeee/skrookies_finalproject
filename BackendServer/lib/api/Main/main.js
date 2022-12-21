b871478753cc86f7c5ad4ffd8b649b802bb057d3
mas615 <mas9623@naver.com>
2022-12-21 23:15:29 +0900

    라우팅,메인,메인인덱스,암호화2

--- BackendServer/lib/api/Main/main.js
+++ BackendServer/lib/api/Main/main.js
@@ -1,22 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var Model = require('../../../models/index');
-var Response = require('../../Response');
-var statusCodes = require('../../statusCodes');
-var { encryptResponse, decryptRequest } = require("../../../middlewares/crypt");
-const jwt = require("jsonwebtoken");
 
-/**
- * Login route
- * This endpoint allows the user to login
- * @path                             - /api/user/login
- * @middleware                       - Checks admin authorization
- * @param username                   - Username to login
- * @param password                   - Password to login
- * @return                           - JWT token
- */
-router.post('/', encryptResponse, (req, res) => {
-    return req;
-});
+
+var html = `
+<html>
+<head>
+  <title>메인페이지</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <h1><a href="/">HOME V17</a></h1>
+      <h3>아이디와 비밀번호를 입력하세요</h3>
+       <form action="/api/user/login" method="post"> // 대상경로지정
+      <p>
+        <input type="text" name="username" placeholder="username">
+      </p>
+      <p>
+        <input type="password" name="password" placeholder="password">
+      </p>
+      <p>
+         <input type="submit">
+         </p>
+         </form>
+         <h1><a href="/">회원가입</a></h1>
+   </body>
+   </html>
+   `;
+
+   app.get('/', (req, res) => res.send(html));
+
 
 module.exports = router;
---
+++ BackendServer/newapp.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const app = express();
+const index = require('./routes/index');
+
+const { encryptResponse, decryptRequest } = require('./middlewares/crypt');
+const Model = require('./models/index');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+var html = `
+<html>
+<head>
+  <title>회원가입</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <h1><a href="/">HOME V17</a></h1>
+
+    <h3>Register-사용자등록</h3>
+  <form action="http://15.168.11.21:3000/api/user/register" method="post">
+    <p>
+      <input type="text" name="username" placeholder="username">
+    </p>
+    <p>
+      <input type="password" name="password" placeholder="password">
+    </p>
+    <p>
+       <input type="submit">
+    </p>
+    </form>
+
+      <h3>TEST.REQ - 리퀘스트 테스트</h3>
+    <form action="/testreq" method="post">
+      <p>
+        <input type="text" name="username" placeholder="username">
+      </p>
+      <p>
+        <input type="password" name="password" placeholder="password">
+      </p>
+      <p>
+         <input type="submit">
+      </p>
+      </form>
+
+      <h3>Login.JWT - 로그인 존맛탱</h3>
+       <form action="/api/user/login" method="post">
+      <p>
+        <input type="text" name="username" placeholder="username">
+      </p>
+      <p>
+        <input type="password" name="password" placeholder="password">
+      </p>
+      <p>
+         <input type="submit">
+         </p>
+         </form>
+
+         <h3>EncryptResponse 시켜보기</h3>
+         <form action="/Encrypt" method="post">
+        <p>
+          <input type="text" name="username" placeholder="username">
+        </p>
+        <p>
+          <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+           <input type="submit">
+           </p>
+           </form>
+   </body>
+   </html>
+   `;
+
+
+   app.get('/', (req, res) => res.send(html));
+
+   app.post('/testreq', (req, res) => res.send(req.body));
+
+   app.get('/list', (req, res) => {
+    const user = Model.users.findOne({});
+    console.log(user);
+    res.send(user.username);
+   }); //리스트
+
+   app.post('/Encrypt', (req, res) => {
+    console.log('Encryptpage')
+        var endbody = encryptResponse(req.body.username + req.body.userpassword);
+        var bodydata =`
+        username : ${req.body.username},
+        password : ${req.body.password},
+        encryptusername : ${endbody}
+        `
+    //console.log(req.body+'success');
+           res.send(endbody)});
+
+   app.use('/api', index);
+
+   module.exports = app;
