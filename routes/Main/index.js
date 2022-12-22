89ed736040675c5d764a26f4471009995d632c4e
dogyun <a2256014@naver.com>
2022-12-22 10:26:23 +0900

    main

---
+++ routes/Main/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+var main = require("./main");
+router.use("/",main)
+
+module.exports = router;
--- routes/User/index.js
+++ routes/User/index.js
@@ -1,14 +1,16 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 //
 // var login = require("./login");
 // var register = require("./register");
 // var profile = require("./profile");
 // var changePassword = require("./changePassword");
+var signup = require("./signup");
 //
 // router.use('/login', login);
 // router.use('/register', register);
 // router.use('/profile', profile);
 // router.use("/change-password", changePassword);
+router.use("/signup", signup);
 //
-// module.exports = router;
+module.exports = router;
