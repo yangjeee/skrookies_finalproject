bf97cf8efcfca9955836be70b13ebca03ffd9f4a
Taejins <69339290+Taejins@users.noreply.github.com>
2022-12-24 20:48:28 +0900

    입출금내역 및 송금 기능 구현 완료

--- routes/Balance/transfer.js
+++ routes/Balance/transfer.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios");
+var { encryptResponse, decryptRequest } = require("../../middlewares/crypt");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render("transfer");
+    res.render("Balance/transfer");
 });
+
+
+router.post('/', function(req, res, next) {
+    let token = req.get('authorization');
+
+    let json_data = {};
+    json_data['to_account'] = parseInt(req.body.to_account);   //데이터가 숫자로 들어가야 동작함
+    json_data['amount'] = parseInt(req.body.amount);
+    
+    const en_data = encryptResponse(JSON.stringify(json_data));// 객체를 문자열로 반환 후 암호화
+    console.log(en_data)
+    // console.log("endata : ",enData)
+
+    axios({
+        method: "post",
+        url: "http://127.0.0.1:3000/api/balance/transfer",
+        headers: {"authorization": token},
+        data: en_data
+    }).then((data)=>{
+        console.log(decryptRequest(data.data));
+    });
+    
+    res.header('authorization',token);
+    return res.redirect("/transactions/view");
+});
+
+
 
 module.exports = router;
--- routes/Transactions/view.js
+++ routes/Transactions/view.js
@@ -3,12 +3,9 @@ var router = express.Router();
 var axios = require("axios");
 var { encryptResponse, decryptRequest } = require("../../middlewares/crypt");
 
-
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
     let token = req.get('authorization'); // 입력값 헤더의 토큰만 받음
-    let result = "{data:[]}";
+
     axios({
         method: "post",
         url: "http://localhost:3000/api/transactions/view", // URL 수정 해야 됨
@@ -16,10 +13,10 @@ router.get('/', function(req, res, next) {
         // data: enData
         // 데이터 안씀
     }).then((data)=>{
-        en_data = decryptRequest(data.data);
-        console.log(en_data);
-        // result = JSON.stringify(en_data); 
-        row = en_data.data;
+        de_data = decryptRequest(data.data);
+        console.log(de_data);
+        // result = JSON.stringify(de_data); 
+        row = de_data.data;
         var html_data = "<table><tr><th>송금자</th><th>수취인</th><th>금액</th></tr>";
 
         row.forEach(function(i){
@@ -34,31 +31,6 @@ router.get('/', function(req, res, next) {
         return res.render("Transactions/view", {html : html_data});
     });
 });
-
-
-
-// /* GET users listing. */
-// router.post('/', function(req, res, next) {
-//     const {username, password, account_number} = req.body;
-//     console.log(username, password, account_number)
-//     baseData=`{"username": "${username}", "password": "${password}", "account_number": "${account_number}"}`
-//     console.log("basedata : ",baseData)
-//     const enData = encryptResponse(baseData);
-
-//     console.log("endata : ",enData)
-//     //balance view
-//     axios({
-//         method: "post",
-//         url: "http://15.152.81.150:3000/api/balance/view",
-//         headers: {"authorization": "1 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEyMyIsImlzX2FkbWluIjpmYWxzZSwiaWF0IjoxNjcxNjg3MjU3fQ.dcCSrCfU6mDJcLGgtib-LxivIWH4Q1PU0Y22lXG9gcA"},
-//         data: enData
-//     }).then((data)=>{
-//         // console.log(decryptRequest(data))
-//         console.log("data : ", decryptRequest(data.data))
-//     })
-
-//     return res.redirect("/")
-// });
 
 
 
