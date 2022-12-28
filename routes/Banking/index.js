ff1fa3841382c67bb800503c5646b9607a9dcc49
majun <akwns615@gmail.com>
2022-12-28 17:12:08 +0900

    친구보기페이지

--- routes/Banking/index.js
+++ routes/Banking/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 //
 var Job = require("./bankJob");
+var friend_list = require("./friend_list");
 
 router.use(express.static( 'public'));
 router.use("/job", Job);
+router.use("/friend_list", friend_list);
 
 module.exports = router;
