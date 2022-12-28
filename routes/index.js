85d949d0e99434a8f20958c5cab7ff2756ff8b01
cksgk97 <cksgk97@naver.com>
2022-12-28 10:43:34 +0900

    3차 코드 정리 (라우트,뷰 정리)

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ const transactionsRouter = require("../routes/Transactions")
 const balanceRouter = require("../routes/Balance");
 const beneficiaryRouter = require("../routes/Beneficiary");
 const userRouter = require("../routes/User");
-const userpage = require("./userpage");
 // const notice = require("../routes/notice");
 // const qna = require("../routes/qna")
 // 1 : /balance 주소, 2: 할당
@@ -20,44 +19,13 @@ router.use("/", mainRouter);
 router.use("/transactions", transactionsRouter)
 router.use("/beneficiary", beneficiaryRouter);
 router.use("/user", userRouter);
-router.use("/mypage", userpage);
 // router.use("/notice", notice);
 // router.use("/qna",qna);
 
 
-// 템플릿 작업용 GET 메소드
-const axios = require("axios");
-const Response = require("../middlewares/Response");
-const {decryptRequest, encryptResponse, decryptEnc} = require("../middlewares/crypt");
 
-router.get('/', function (req, res, next) {
-    if(req.cookies.Token){
-        const cookie = decryptEnc(req.cookies.Token);
-        axios({
-            method: "post",
-            url: "http://15.152.81.150:3000/api/User/profile",
-            headers: {"authorization": "1 " + cookie}
-        }).then((data) => {
-            // console.log(data.data);
-            const r = new Response();
-            const resStatus = decryptRequest(data.data).status;
-            const resData = decryptRequest(data.data).data;
-            console.log(resData);
-            console.log("------------------");
-            console.log("status : ", resStatus, "data : ", resData);
-            console.log("------------------");
-            console.log(r);
-            r.status = resStatus
-            r.data = resData
-            console.log(r.data);
-            res.render("temp/index", {u_data: r.data.username});    
-        });
-    }
-    else{
-        res.render("temp/index", );
-    }
-    // res.render("temp/index", {in_data: false});
-});
+
+
 
 
 
