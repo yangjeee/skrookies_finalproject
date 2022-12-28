195aa4d6ad0533e09378c10822b82c7627517f68
dogyun <a2256014@naver.com>
2022-12-28 17:49:08 +0900

    통합

--- routes/Banking/index.js
+++ routes/Banking/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 //
-var Job = require("./bankJob");
 var list = require("./trade_list");
 var send = require("./trade_send");
 var friend_list = require("./friend_list");
@@ -9,7 +8,6 @@ var user_register =require("./user_register");
 var admin = require("./admin")
 
 router.use(express.static( 'public'));
-router.use("/job", Job);
 router.use("/admin", admin)
 router.use("/user_register", user_register);
 router.use("/friend_list", friend_list);
