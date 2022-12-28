3bab76ec4a19f14cd30fb8bc45041fbdc4a84d61
cksgk97 <cksgk97@naver.com>
2022-12-28 17:14:47 +0900

    no message

--- routes/Banking/index.js
+++ routes/Banking/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 //
 var Job = require("./bankJob");
+var user_register =require("./user_register");
 
 router.use(express.static( 'public'));
 router.use("/job", Job);
+router.use("/user_register", user_register);
 
 module.exports = router;
