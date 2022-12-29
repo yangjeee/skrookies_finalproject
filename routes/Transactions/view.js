3be6fd9ed58e0714a42b50bd64bb072bbad3bec5
dogyun <a2256014@naver.com>
2022-12-29 13:57:53 +0900

    url fix

--- routes/Transactions/view.js
+++ routes/Transactions/view.js
@@ -1,36 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var axios = require("axios");
-var { encryptResponse, decryptRequest, decryptEnc} = require("../../middlewares/crypt");
+var {encryptResponse, decryptRequest, decryptEnc} = require("../../middlewares/crypt");
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     const cookie = decryptEnc(req.get("cookie").split("Token=")[1])
     axios({
         method: "post",
-        url: "http://15.152.81.150:3000/api/transactions/view", // URL 수정 해야 됨
+        url: api_url + "/api/transactions/view", // URL 수정 해야 됨
         headers: {"authorization": "1 " + cookie}
         // data: enData
         // 데이터 안씀
-    }).then((data)=>{
+    }).then((data) => {
         de_data = decryptRequest(data.data);
         console.log(de_data);
         // result = JSON.stringify(de_data); 
         row = de_data.data;
         var html_data = "<table><tr><th>송금자</th><th>수취인</th><th>금액</th></tr>";
 
-        row.forEach(function(i){
-            html_data += "<tr><td>"+i.from_account+"</td><td>"+i.to_account+"</td><td>"+i.amount+"</td></tr>";
+        row.forEach(function (i) {
+            html_data += "<tr><td>" + i.from_account + "</td><td>" + i.to_account + "</td><td>" + i.amount + "</td></tr>";
         })
 
         html_data += "</table>";
-        return res.render("Transactions/view", {html : html_data});
+        return res.render("Transactions/view", {html: html_data});
 
-    }).catch(function(error){
+    }).catch(function (error) {
         var html_data = "<h2>내용을 불러올 수 없습니다.</h2>";
-        return res.render("Transactions/view", {html : html_data});
+        return res.render("Transactions/view", {html: html_data});
     });
 });
-
 
 
 module.exports = router;
