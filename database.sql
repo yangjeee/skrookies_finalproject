fcfc7187a694de7e31fd40920cb2722e59a96db9
dogyun <a2256014@naver.com>
2022-12-29 16:48:46 +0900

    1

--- database.sql
+++ database.sql
@@ -39,29 +39,17 @@ create table boards
     PRIMARY KEY (id)
 ) engine = innodb;
 
-INSERT INTO `users`
-values (default, "user1", "password1", 111111, default, default);
-INSERT INTO `users`
-values (default, "user2", "password2", 222222, default, default);
-INSERT INTO `users`
-values (default, "user3", "password3", 333333, default, default);
-INSERT INTO `users`
-values (default, "user4", "password4", 444444, default, default);
-INSERT INTO `users`
-values (default, "admin", "admin", 999999, default, true);
+create table qna
+(
+    id        int not null auto_increment,
+    userId    varchar(30),
+    title     VARCHAR(255),
+    content   VARCHAR(255),
+    filepath   VARCHAR(255),
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    PRIMARY KEY (id)
+) engine = innodb;
 
-INSERT INTO `transactions`
-values (default, 111111, 222222, 100);
-INSERT INTO `transactions`
-values (default, 222222, 111111, 200);
-INSERT INTO `transactions`
-values (default, 111111, 333333, 100);
-INSERT INTO `transactions`
-values (default, 111111, 444444, 100);
-
-INSERT INTO `beneficiaries`
-values (default, 111111, 222222, true);
-INSERT INTO `beneficiaries`
-values (default, 111111, 333333, true);
-INSERT INTO `beneficiaries`
-values (default, 111111, 444444, true);
+INSERT INTO `users`
+values (default, "admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", 999999, default, true);
