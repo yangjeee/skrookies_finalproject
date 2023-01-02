ee6260f16e25ff55eb6dfeaf034917114a051ab9
Mike Lue <chocohummingbird@gmail.com>
2023-01-02 23:12:50 +0900

    진짜 바꿈

--- routes/notice/viewBoard.js
+++ routes/notice/viewBoard.js
@@ -1,32 +1,34 @@
 // const { Boards } = require('../../models');
-var db = require('../../middlewares/db');
-var express = require('express');
+var db = require("../../middlewares/db");
+var express = require("express");
 var router = express.Router();
-var tokenauth = require('./tokenauth');
-var {encryptResponse, decryptRequest, decryptEnc} = require("../../middlewares/crypt");
-const profile = require('../../middlewares/profile');
-const checkCookie = require("../../middlewares/checkCookie")
+var tokenauth = require("./tokenauth");
+var {
+  encryptResponse,
+  decryptRequest,
+  decryptEnc,
+} = require("../../middlewares/crypt");
+const profile = require("../../middlewares/profile");
 
-router.get('/', checkCookie, function (req, res, next) {
-    const cookie = req.cookies.Token;
+router.get("/", function (req, res, next) {
+  db.query(`SELECT * FROM boards`, function (error, results) {
+    if (error) {
+      throw error;
+    }
 
-    profile(cookie).then((data) => {
+    if (req.cookies.Token) {
+      const cookie = decryptEnc(req.cookies.Token);
+      profile(cookie).then((data) => {
         var cookieData = data.data;
-        tokenauth.authresult(req, function (aResult) {
-            if (aResult == true) {
-                db.query(`SELECT *
-                          FROM boards`, function (error, results) {
-                    if (error) {
-                        throw error;
-                    }
-                    res.render('temp/notice/viewboard', {u_data: cookieData.username, results: results});
-                });
-            } else {
-                res.render('temp/notice/alert');
-            }
+        res.render("temp/notice/viewboard", {
+          u_data: cookieData.username,
+          results: results,
         });
-    })
-
+      });
+    } else {
+      res.render("temp/notice/viewboard", { results: results });
+    }
+  });
 });
 
 module.exports = router;
--- routes/qna/viewBoard.js
+++ routes/qna/viewBoard.js
@@ -1,32 +1,40 @@
-var db = require('../../middlewares/db');
-var express = require('express');
+var db = require("../../middlewares/db");
+var express = require("express");
 var router = express.Router();
-var tokenauth = require('./tokenauth');
-var {encryptResponse, decryptRequest, decryptEnc} = require("../../middlewares/crypt");
-const profile = require('../../middlewares/profile');
-const checkCookie = require("../../middlewares/checkCookie")
+var tokenauth = require("./tokenauth");
+var {
+  encryptResponse,
+  decryptRequest,
+  decryptEnc,
+} = require("../../middlewares/crypt");
+const profile = require("../../middlewares/profile");
+const checkCookie = require("../../middlewares/checkCookie");
 
-router.get('/', checkCookie, function (req, res, next) {
-    const cookie = req.cookies.Token;
-    profile(cookie).then((data) => {
-        var cookieData = data.data;
-        tokenauth.authresult(req, function (aResult) {
-
-            if (aResult == true) {
-
-                var userid = cookieData.username;
-                db.query(`SELECT *
-                          FROM qna Where userId = '${userid}'`, function (error, results) {
-                    if (error) {
-                        throw error;
-                    }
-                    res.render('temp/qna/viewboard', {results: results, u_data: cookieData.username});
-                });
-            } else {
-                res.render('temp/qna/alert');
+router.get("/", checkCookie, function (req, res, next) {
+  const cookie = req.cookies.Token;
+  profile(cookie).then((data) => {
+    var cookieData = data.data;
+    tokenauth.authresult(req, function (aResult) {
+      if (aResult == true) {
+        var userid = cookieData.username;
+        db.query(
+          `SELECT *
+                          FROM qna Where userId = '${userid}'`,
+          function (error, results) {
+            if (error) {
+              throw error;
             }
-        });
+            res.render("temp/qna/viewboard", {
+              results: results,
+              u_data: cookieData.username,
+            });
+          }
+        );
+      } else {
+        res.render("temp/qna/alert");
+      }
     });
+  });
 });
 
 module.exports = router;
