043eb372a7baf178affaa9dcd6112633149c8f3b
LAPTOP-A96P6D2C\user <mikeipodfan@gmail.com>
2023-01-06 14:35:53 +0900

    오류해결

--- routes/notice/editBoard.js
+++ routes/notice/editBoard.js
@@ -3,48 +3,56 @@ var seoultime = require('../../middlewares/seoultime');
 var express = require('express');
 var router = express.Router();
 var tokenauth = require('./tokenauth');
-var {encryptResponse, decryptRequest, decryptEnc} = require("../../middlewares/crypt");
+var {
+    encryptResponse,
+    decryptRequest,
+    decryptEnc
+} = require("../../middlewares/crypt");
 const profile = require('../../middlewares/profile');
 const multer = require('multer')
 const checkCookie = require("../../middlewares/checkCookie")
 const path = require('path');
 const fs = require('fs');
 const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, req.body.fid);
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
+    storage: multer.diskStorage(
+        {
+            destination: function (req, file, cb) {
+                cb(null, req.body.fid);
+            },
+            filename: function (req, file, cb) {
+                cb(null, file.originalname);
+            }
+        }
+    )
 });
 
 router.get('/', function (req, res, next) {
-    if(req.cookies.Token){
-    var cookie = decryptEnc(req.cookies.Token);
-    profile(cookie).then((data) => {
-        var cookieData = data.data;
-        tokenauth.admauthresult(req, function (aResult) {
-            if (aResult == true) {
-                db.query(`SELECT *
+    if (req.cookies.Token) {
+        var cookie = decryptEnc(req.cookies.Token);
+        profile(cookie).then((data) => {
+            var cookieData = data.data;
+            tokenauth.admauthresult(req, function (aResult) {
+                if (aResult == true) {
+                    db.query(`SELECT *
                           FROM notice
-                          where id = ${req.query.id}`, function (error, results) {
-                    if (error) {
-                        throw error;
-                    }
-                    res.render('temp/notice/editBoard', {
-                        results: results,
-                        u_data: cookieData.username,
-                        tempid: req.query.id
+                          where id = ${
+                        req.query.id
+                    }`, function (error, results) {
+                        if (error) {
+                            throw error;
+                        }
+                        res.render('temp/notice/editBoard', {
+                            results: results,
+                            u_data: cookieData.username,
+                            tempid: req.query.id
+                        });
                     });
-                });
-            } else {
-                res.render('temp/notice/alert');
-            }
+                } else {
+                    res.render('temp/notice/alert');
+                }
+            });
         });
-    });
-}else{
+    } else {
         res.render('temp/notice/alert');
     }
 });
@@ -82,16 +90,16 @@ router.post('/edit', checkCookie, upload.single("imgimg"), function (req, res, n
 
         const {title, contents, pid, deletepath} = req.body;
 
-    db.query(`UPDATE notice
+        db.query(`UPDATE notice
               SET title     = '${title}',
                   content   = '${contents}',
                   updatedAt = '${seoultime}'
               WHERE id = ${pid}`, function (error, results) {
-        if (error) {
-            throw error;
-        }
-        res.redirect('../viewBoard');
-    });
+            if (error) {
+                throw error;
+            }
+            res.redirect('../viewBoard');
+        });
     }
 });
 
