b39a934a0720df10fc54fad280de398ad47227fd
cksgk97 <cksgk97@naver.com>
2023-01-06 09:51:32 +0900

    비밀번호 변경

--- routes/User/changePass.js
+++ routes/User/changePass.js
@@ -33,14 +33,14 @@ router.post("/", checkCookie, (req, res) => {
     const {password, new_password} = req.body
     const sha256Pass = sha256(password)
     const sha256Newpass = sha256(new_password)
-    const req_data = `{"password" : ${sha256Pass},"new_password" : ${sha256Newpass}}`
+    const req_data = `{"password" : "${sha256Pass}","new_password" : "${sha256Newpass}"}`
     const cookie = req.cookies.Token;
     let resStatus = ""
     let resMessage = ""
-
+    console.log(req.body)
     axios({
         method: "post",
-        url: api_url + "/api/user/change-password",
+        url: api_url + "/api/User/change-password",
         headers: {"authorization": "1 " + cookie},
         data: encryptResponse(req_data)
     }).then((data) => {
@@ -48,7 +48,7 @@ router.post("/", checkCookie, (req, res) => {
         resMessage = decryptRequest(data.data).data.message
         console.log(resStatus, resMessage)
         if (resStatus.code === 200) {
-            res.redirect("/")
+            return res.send("<script>alert('비밀번호가 변경되었습니다.');location.href = \"/user/login\";</script>");
         } else {
             console.log(resMessage)
             res.render("temp/changePass", {message: resMessage})
