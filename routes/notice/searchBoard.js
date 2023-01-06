35b75f364f2ada66110e022f62daa88ca9e28ae1
LAPTOP-A96P6D2C\user <mikeipodfan@gmail.com>
2023-01-06 17:14:31 +0900

    search 오류 고침

--- routes/notice/searchBoard.js
+++ routes/notice/searchBoard.js
@@ -3,37 +3,46 @@ var express = require("express");
 var router = express.Router();
 var tokenauth = require("./tokenauth");
 var {
-  encryptResponse,
-  decryptRequest,
-  decryptEnc,
+    encryptResponse,
+    decryptRequest,
+    decryptEnc
 } = require("../../middlewares/crypt");
 const profile = require("../../middlewares/profile");
 const checkCookie = require("../../middlewares/checkCookie");
 
 router.post("/", function (req, res, next) {
-  var cookie = decryptEnc(req.cookies.Token);
-  profile(cookie).then((data) => {
-    var cookieData = data.data;
-    tokenauth.authresult(req, function (aResult) {
-      if (aResult == true) {
-        db.query(
-          `SELECT *
-                          FROM notice Where title LIKE '%${req.body.searchTitle}%'`,
-          function (error, results) {
-            if (error) {
-              throw error;
-            }
-            res.render("temp/notice/viewboard", {
-              results: results,
-              u_data: cookieData.username,
+    if (req.cookies.Token) {
+        var cookie = decryptEnc(req.cookies.Token);
+        profile(cookie).then((data) => {
+            var cookieData = data.data;
+            db.query(`SELECT *
+                          FROM notice Where title LIKE '%${
+                req.body.searchTitle
+            }%'`, function (error, results) {
+                if (error) {
+                    throw error;
+                }
+                res.render("temp/notice/viewboard", {
+                    results: results,
+                    u_data: cookieData.username
+                });
             });
-          }
-        );
-      } else {
-        res.render("temp/notice/alert");
-      }
-    });
-  });
+
+        });
+    } else {
+        db.query(`SELECT *
+                    FROM notice Where title LIKE '%${
+            req.body.searchTitle
+        }%'`, function (error, results) {
+            if (error) {
+                throw error;
+            }
+            res.render("temp/notice/viewboard", {results: results});
+        });
+
+    }
+
 });
+
 
 module.exports = router;
