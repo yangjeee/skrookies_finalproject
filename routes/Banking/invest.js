124863cdb8bc9548c4ebdfaf6227f3e4665eb5e8
dogyun <a2256014@naver.com>
2023-01-10 17:54:16 +0900

    margin

--- routes/Banking/invest.js
+++ routes/Banking/invest.js
@@ -47,47 +47,21 @@ router.get('/', function (req, res, next) {
 
     profile(decryptEnc(cookie)).then((data) => {
         var cookieData = data.data;
-        tokenauth.authresult(req, function (aResult) {
+        var margin = "";
+        db.query(`SELECT * FROM margin where userId='${data.data.username}'`,function (error, marginData) {
+            if(marginData.length !== 0){
+                margin = marginData[0].margin
+            }
+            tokenauth.authresult(req, function (aResult) {
 
-            if (aResult == true) {
-                var gachabt = ` <thead>
+                if (aResult == true) {
+                    var gachabt = ` <thead>
                                     <tr>
-                                    
-                                    <th>
-                                        <form id="1" action="/bank/gacha" method="post">
-                                        <input type="hidden" name="gacha" value="500"/>
-                                        <input type="hidden" name="account_number" value="${data.data.account_number}"/>
-                                        <input type="hidden" name="balance" value="${data.data.balance}"/>  
-                                        <a onclick="document.getElementById('1').submit();" class="btn btn-google btn-user btn-block">
-                                        1,000달러 행운의상자
-                                        </a>
-                                        </form>
-                                    </th>
-                                    <th>
-                                        <form id="2" action="/bank/gacha/10000" method="post">
-                                        <input type="hidden" name="gacha" value="10000"/>
-                                        <input type="hidden" name="account_number" value="${data.data.account_number}"/>
-                                        <input type="hidden" name="balance" value="${data.data.balance}"/>  
-                                        <a onclick="document.getElementById('2').submit();" class="btn btn-google btn-user btn-block">
-                                        10,000달러 축복의상자
-                                        </a>
-                                        </form>
-                                    </th>
-                                    <th>
-                                        <form id="3" action="/bank/gacha/100000" method="post">
-                                        <input type="hidden" name="gacha" value="100000"/>
-                                        <input type="hidden" name="account_number" value="${data.data.account_number}"/>
-                                        <input type="hidden" name="balance" value="${data.data.balance}"/>  
-                                        <a onclick="document.getElementById('3').submit();" class="btn btn-google btn-user btn-block">
-                                        100,000달러 마법의 상자
-                                        </a>
-                                        </form>
-                                    </th>
-
+                                        <td>수익 : ${margin} $</td>
                                     </tr>
                                     </thead>  `
 
-                var html_data = `<thead>
+                    var html_data = `<thead>
                                 <tr>
                                 <th>종목</th>
                                 <th>현재가격</th>
@@ -97,15 +71,15 @@ router.get('/', function (req, res, next) {
                             
                             <tbody>
                             `;
-                let priceList = {}
-                investList.forEach((x, index) => {
-                    let name = ""
-                    name = naming(x.id)
-                    x.value.then(value => {
-                        // 034730 sk 105560 kb 005930 삼성
-                        priceList[x.id] = value.now
+                    let priceList = {}
+                    investList.forEach((x, index) => {
+                        let name = ""
+                        name = naming(x.id)
+                        x.value.then(value => {
+                            // 034730 sk 105560 kb 005930 삼성
+                            priceList[x.id] = value.now
 
-                        html_data += `<tr>
+                            html_data += `<tr>
                                 <td>${name}</td>
                                 <td>${value.now}</td>
                                 <td>
@@ -119,10 +93,10 @@ router.get('/', function (req, res, next) {
                                 </form>
                                 </td>
                             </tr>`;
-                        if (index === investList.length - 1) {
-                            html_data += `</tbody>`;
+                            if (index === investList.length - 1) {
+                                html_data += `</tbody>`;
 
-                            var html_data2 = `<thead>
+                                var html_data2 = `<thead>
                                 <tr>
                                 <th>종목</th>
                                 <th>산가격</th>
@@ -134,12 +108,12 @@ router.get('/', function (req, res, next) {
                             <tbody>
                             `;
 
-                            setTimeout(function () {
-                                db.query(`SELECT *
+                                setTimeout(function () {
+                                    db.query(`SELECT *
                                       FROM invest
                                       WHERE userId = '${data.data.username}'`, function (error, resultInvest) {
-                                    resultInvest.forEach(list => {
-                                        html_data2 += `<tr>
+                                        resultInvest.forEach(list => {
+                                            html_data2 += `<tr>
                                     <td>${naming(list.investId)}</td>
                                     <td>${list.curPrice}</td>
                                     <td>${list.buyCount}</td>
@@ -148,31 +122,33 @@ router.get('/', function (req, res, next) {
                                     <input type="hidden" value="${list.buyCount}" name="Sellcount"/>
                                     <input type="hidden" name="userId" value="${data.data.username}"/>
                                     <input type="hidden" name="id" value="${list.id}"/>
+                                    <input type="hidden" name="myCurPrice" value="${list.curPrice}"/>
                                     <input type="hidden" name="curPrice" value="${priceList[list.investId]}"/>
                                     <input type="hidden" name="account_number" value="${data.data.account_number}"/>
                                     <button type="submit">판매</button>
                                     </form>
                                     </td>
                                     </tr>`;
+                                        })
+                                        res.render('Banking/gacha', {
+                                            // results: results,
+                                            u_data: cookieData.username,
+                                            pending: data,
+                                            select: "gacha",
+                                            html: html_data,
+                                            html2: html_data2,
+                                            gachabt: gachabt
+                                        });
                                     })
-                                    res.render('Banking/gacha', {
-                                        // results: results,
-                                        u_data: cookieData.username,
-                                        pending: data,
-                                        select: "gacha",
-                                        html: html_data,
-                                        html2: html_data2,
-                                        gachabt: gachabt
-                                    });
-                                })
-                            }, 20)
-                        }
+                                }, 20)
+                            }
+                        })
                     })
-                })
-            } else {
-                res.render('temp/qna/alert');
-            }
-        });
+                } else {
+                    res.render('temp/qna/alert');
+                }
+            });
+        })
     });
 });
 
@@ -180,15 +156,17 @@ router.post('/invest', checkCookie, function (req, res, next) {
     var account_number = req.body.account_number;
     var userId = req.body.userId;
     var investId = req.body.investId;
-    var curPrice = req.body.curPrice;
     var Buycount = req.body.Buycount;
-    var Calculation = curPrice * Buycount;
 
-    db.query(`UPDATE users
+    api(investId).then(data=>{
+        var curPrice = data.now
+        var Calculation = curPrice * Buycount;
+        db.query(`UPDATE users
               SET balance=balance - ${Calculation}
               where account_number = ${account_number}`);  //주식 가격만큼 돈에서 차감
-    db.query(`INSERT INTO invest(userId, investId, curPrice, buyCount)
+        db.query(`INSERT INTO invest(userId, investId, curPrice, buyCount)
               VALUES ('${userId}', '${investId}', ${curPrice}, ${Buycount})`); //주식 테이블에 구매자료 입력
+    })
     res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
     res.write("<script>alert('주식 구매 완료')</script>");
     res.write("<script>window.location='/bank/invest'</script>");
@@ -196,16 +174,22 @@ router.post('/invest', checkCookie, function (req, res, next) {
 
 router.post('/sell', checkCookie, function (req, res, next) {
     var account_number = req.body.account_number;
+    var myCurPrice = req.body.myCurPrice;
     var curPrice = req.body.curPrice;
     var Sellcount = req.body.Sellcount;
     var id = req.body.id;
+    var userId = req.body.userId;
     var Calculation = curPrice*Sellcount;
+
+    var magin = (curPrice-myCurPrice)*Sellcount;
 
     db.query(`DELETE FROM invest WHERE id=${id}`); //invest 테이블에서 제거
     db.query(`UPDATE users SET balance=balance+${Calculation} where account_number=${account_number}`);  //주식가격만큼 돈 상승
+    db.query(`INSERT INTO margin (userId, margin) VALUES ('${userId}', ${magin}) ON DUPLICATE KEY UPDATE userId='${userId}', margin=margin+${magin};`);
     res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
     res.write("<script>alert('주식 판매 완료')</script>");
     res.write("<script>window.location='/bank/invest'</script>");
 });
+
 
 module.exports = router;
