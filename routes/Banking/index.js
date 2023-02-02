867660bcd3650cb5799e48e351e876c1451392d2
LAPTOP-A96P6D2C\user <mikeipodfan@gmail.com>
2023-02-02 09:48:22 +0900

    날짜검색 오류 수정

--- routes/Banking/index.js
+++ routes/Banking/index.js
@@ -6,17 +6,12 @@ var send = require("./trade_send");
 var friend_list = require("./friend_list");
 var user_register = require("./user_register");
 var admin = require("./admin");
-var search = require("./search");
-var searchc = require("./searchc");
 
 router.use(express.static("public"));
-
 router.use("/admin", admin);
 router.use("/user_register", user_register);
 router.use("/friend_list", friend_list);
 router.use("/list", list);
 router.use("/send", send);
-router.use("/search", search);
-router.use("/searchc", searchc);
 
 module.exports = router;
--- routes/Banking/trade_list.js
+++ routes/Banking/trade_list.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const axios = require("axios");
 const profile = require("../../middlewares/profile")
-const {decryptRequest} = require("../../middlewares/crypt")
+const {decryptRequest, encryptResponse, decryptEnc} = require("../../middlewares/crypt")
 const checkCookie = require("../../middlewares/checkCookie")
-var {simpletime} = require('../../middlewares/seoultime');
+var {seoultime, simpletime} = require('../../middlewares/seoultime');
 
 router.get("/", checkCookie, async (req, res) => {
     var row = global.realdata;
@@ -39,8 +39,8 @@ router.get("/", checkCookie, async (req, res) => {
                     <tr><th>송금자</th><th>수취인</th><th>금액</th><th>시간</th></tr></thead>`;
 
                     if(data2){
-                        de_data = decryptRequest(data2.data);
-                        row = de_data.data.result;
+                          de_data = decryptRequest(data2.data);
+                    row = de_data.data.result;
                         row.forEach(function (i) {
                             var temp = i.sendtime ;
                             html_data += "<tr><td>" + i.from_account + "</td><td>" + i.to_account + "</td><td>" + i.amount + "</td><td>" + temp.substring(0, temp.length - 5); + "</td></tr>" ;
@@ -75,32 +75,18 @@ router.post("/", checkCookie, async (req, res) => {
             // data: enData
             // 데이터 안씀
         }).then((data2) => {
-            let de_data = decryptRequest(data2.data);
-            let row = de_data.data;
-            var html_data = ` <form id="send" class="get" action="/bank/search" method="POST">
-            검색시작일&nbsp;&nbsp;<input type="date" id="start" name="tripstart"
-            value="${simpletime}"
-            min="2023-01-01" max="${simpletime}">&nbsp;&nbsp;
-            검색종료일&nbsp;&nbsp;<input type="date" id="end" name="tripend"
-            value="${simpletime}"
-            min="2023-01-01" max="${simpletime}">&nbsp;&nbsp;
-            <input type ="submit" value ="검색">
-            </form>
-            <tr><th>송금자</th><th>수취인</th><th>금액</th><th>시간</th></tr></thead>`;
+            de_data = decryptRequest(data2.data);
+            global.realdata = de_data.data.result
+            console.log("글로벌"+global.realdata)
+            return res.redirect("/bank/list");
 
-            row.forEach(function (i) {
-                var temp = i.sendtime;
-                html_data += "<tr><td>" + i.from_account + "</td><td>" + i.to_account + "</td><td>" + i.amount + "</td><td>" + temp.substring(0, temp.length - 5);
-                +"</td></tr>";
-            })
-
-            return res.render("Banking/trade_list", {pending: data, html: html_data, select: "list"});
         }).catch(function (error) {
-            var html_data = "<h2>내용을 불러올 수 없습니다.</h2>";
-
-            return res.render("Banking/trade_list", {pending: data, html: html_data, select: "list"});
+            return res.redirect("/bank/list");
         });
-    })
+
+        // res.render("Banking/trade_list",{pending:data})
+    });
+
 });
     
 module.exports = router;
